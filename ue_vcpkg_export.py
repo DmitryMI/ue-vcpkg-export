af9b97353c088de4e582dff60ff4a0ea4006a045
Dmitriy Monakhov <dimamon6032403@gmail.com>
2024-04-06 16:22:19 +0200

    Working version

--- ue_vcpkg_export.py
+++ ue_vcpkg_export.py
@@ -1,5 +1,6 @@
 import argparse
 import fnmatch
+import json
 import os
 import re
 import shutil
@@ -11,16 +12,58 @@ from templated_string import TemplatedString
 
 
 class UeVcpkgExport:
-    def __init__(self, package: str, vcpkg_dir: str, packages_dict: dict, logger_obj=None):
+    class PackageRule:
+        def __init__(
+                self,
+                package_vcpkg_name,
+                package_rules_all: dict
+        ):
+            trimmed_name = UeVcpkgExport.trim_vcpkg_name(package_vcpkg_name)
+            self.do_export = True
+            self.do_reference = True
+            self.export_name = UeVcpkgExport.generate_camel_case_module_name(trimmed_name)
+            self.reference_name = self.export_name
+
+            rules = UeVcpkgExport.PackageRule._find_package_rule(package_vcpkg_name, package_rules_all)
+            if rules is not None:
+                self.do_export = rules["do_export"] if "do_export" in rules else self.do_export
+                self.do_reference = rules["do_reference"] if "do_reference" in rules else self.do_reference
+                self.export_name = rules["export_name"] if "export_name" in rules else self.export_name
+                self.reference_name = rules["reference_name"] if "reference_name" in rules else self.reference_name
+
+        @staticmethod
+        def _find_package_rule(package_name: str, package_rules: dict):
+            for package_regex, rules in package_rules.items():
+                if re.match(package_regex, package_name):
+                    return rules
+            return None
+
+    def __init__(
+            self,
+            package: str,
+            vcpkg_dir: str,
+            packages_dict: dict,
+            package_rules: dict | None,
+            triplets: list[str],
+            fn_pattern: list[str],
+            extension_filter: list[str],
+            logger_obj=None
+    ):
         self._package = package
         self._packages_dict = packages_dict
         self._vcpkg_dir = vcpkg_dir
         self._logger = logger_obj or logging.getLogger("UeVcpkgExport")
-        self._dependencies = []
+        self._dependencies: list[UeVcpkgExport] = []
         self._vcpkg_exe = os.path.join(self._vcpkg_dir, "vcpkg.exe")
 
         if self._package not in self._packages_dict:
             self._packages_dict[self._package] = self
+
+        self._package_rules = package_rules
+
+        self._fn_pattern = fn_pattern
+        self._extension_filter = extension_filter
+        self._triplets = triplets
 
         self._walk_dependencies()
 
@@ -49,8 +92,25 @@ class UeVcpkgExport:
             if dependency_package in self._packages_dict:
                 self._dependencies.append(self._packages_dict[dependency_package])
             else:
-                exporter = UeVcpkgExport(dependency_package, self._vcpkg_dir, self._packages_dict, self._logger)
+                exporter = UeVcpkgExport(
+                    dependency_package,
+                    self._vcpkg_dir,
+                    self._packages_dict,
+                    self._package_rules,
+                    self._triplets,
+                    self._fn_pattern,
+                    self._extension_filter,
+                    self._logger
+                )
                 self._dependencies.append(exporter)
+
+    @staticmethod
+    def trim_vcpkg_name(vcpkg_name: str):
+        if "[" not in vcpkg_name:
+            return vcpkg_name
+
+        bracket_index = vcpkg_name.index("[")
+        return vcpkg_name[:bracket_index]
 
     @staticmethod
     def _get_dynamic_library_extension(triplet: str):
@@ -88,9 +148,8 @@ class UeVcpkgExport:
     def _generate_public_include_paths(triplet: str):
         return [f"Path.Combine(ModuleDirectory, \"{triplet}\", \"include\")"]
 
-    @staticmethod
-    def _generate_public_definitions(package_name: str):
-        text = f"\"WITH_{package_name.upper()}\""
+    def _generate_public_definitions(self):
+        text = f"\"WITH_{self.get_module_export_name().upper()}\""
         return [text]
 
     @staticmethod
@@ -99,8 +158,24 @@ class UeVcpkgExport:
         return output[0].upper() + output[1:]
 
     @staticmethod
-    def _generate_module_name(package_name: str):
+    def generate_camel_case_module_name(package_name: str):
         return UeVcpkgExport._get_camel_case(package_name)
+
+    def get_module_export_name(self):
+        rule = UeVcpkgExport.PackageRule(self._package, self._package_rules)
+        return rule.export_name
+
+    def get_module_reference_name(self):
+        rule = UeVcpkgExport.PackageRule(self._package, self._package_rules)
+        return rule.reference_name
+
+    def should_reference_module(self):
+        rule = UeVcpkgExport.PackageRule(self._package, self._package_rules)
+        return rule.do_reference
+
+    def should_export_module(self):
+        rule = UeVcpkgExport.PackageRule(self._package, self._package_rules)
+        return rule.do_export
 
     @staticmethod
     def _generate_csharp_dictionary_record(key: str, value: list[str] | str):
@@ -146,7 +221,7 @@ class UeVcpkgExport:
             return None
 
     def _generate_triplet_file_record(self, triplet: str, triplet_files: list[str]):
-        defs = UeVcpkgExport._generate_public_definitions(self._package)
+        defs = self._generate_public_definitions()
         includes = UeVcpkgExport._generate_public_include_paths(triplet)
         libs = UeVcpkgExport._generate_public_library_dependencies(triplet, triplet_files)
         dlls = UeVcpkgExport._generate_runtime_dependencies(triplet, triplet_files)
@@ -169,16 +244,19 @@ class UeVcpkgExport:
             module_build_template = TemplatedString(file_in.read())
 
         triplet_file_records = []
-        for triplet, triplet_files in triplet_files.items():
-            triplet_file_record = self._generate_triplet_file_record(triplet, triplet_files)
+        for triplet, files in triplet_files.items():
+            triplet_file_record = self._generate_triplet_file_record(triplet, files)
             triplet_file_records.append(triplet_file_record)
 
         triplet_file_records_csharp = UeVcpkgExport._generate_csharp_array(triplet_file_records)
-
+        dependency_module_names = self._generate_dependency_module_names()
+        dependency_module_loader_names = self._generate_dependency_module_loader_names()
         build_cs = module_build_template.substitute(
             {
-                "ModuleName": UeVcpkgExport._generate_module_name(self._package),
-                "TripletFileRecords": triplet_file_records_csharp
+                "ModuleName": self.get_module_export_name(),
+                "TripletFileRecords": triplet_file_records_csharp,
+                "DependencyModuleNames": dependency_module_names,
+                "DependencyModuleLoaderNames": dependency_module_loader_names
             }
         )
 
@@ -191,7 +269,7 @@ class UeVcpkgExport:
 
         build_cs = module_loader_build_template.substitute(
             {
-                "ModuleName": UeVcpkgExport._generate_module_name(self._package),
+                "ModuleName": self.get_module_export_name(),
             }
         )
 
@@ -204,7 +282,7 @@ class UeVcpkgExport:
         return platform[0].upper() + platform[1:]
 
     @staticmethod
-    def _is_loader_module_needed(triplet_files:  dict[str, list[str]]):
+    def _is_loader_module_needed(triplet_files: dict[str, list[str]]):
         for triplet, files in triplet_files.items():
             ext = UeVcpkgExport._get_dynamic_library_extension(triplet)
             dlls = list(UeVcpkgExport._get_files_extension_filter(files, ext))
@@ -212,6 +290,17 @@ class UeVcpkgExport:
                 return True
 
         return False
+
+    def get_triplet_files(self):
+        triplet_files = {}
+        for triplet in self._triplets:
+            owned_files = self.get_owned_files(triplet, self._fn_pattern, self._extension_filter)
+            if not owned_files:
+                self._logger.error(f"Package {self._package} has no owned files for triplet {triplet}!")
+                quit(1)
+
+            triplet_files[triplet] = owned_files
+        return triplet_files
 
     @staticmethod
     def _generate_dynamic_library_binary_names(triplet_files: dict[str, list[str]]):
@@ -225,16 +314,54 @@ class UeVcpkgExport:
 
         return "{{" + ", ".join(map_items) + "}}"
 
+    def get_package(self):
+        return self._package
+
+    def __repr__(self):
+        return f"UeVcpkgExport[package: {self._package}]"
+
+    def __str__(self):
+        return f"UeVcpkgExport[package: {self._package}]"
+
+    def _generate_dependency_module_names(self):
+        names = set()
+        for dependency_obj in self._dependencies:
+            if not dependency_obj.should_reference_module():
+                continue
+
+            module_name = dependency_obj.get_module_reference_name()
+            names.add(f"\"{module_name}\"")
+        return ", ".join(names)
+
+    def _generate_dependency_module_loader_names(self):
+        names = set()
+        for dependency_obj in self._dependencies:
+            if not dependency_obj.should_reference_module():
+                continue
+
+            if not dependency_obj.should_export_module():
+                continue
+
+            dependency_triplet_files = dependency_obj.get_triplet_files()
+
+            if not UeVcpkgExport._is_loader_module_needed(dependency_triplet_files):
+                continue
+
+            module_name = dependency_obj.get_module_reference_name()
+            names.add(f"\"{module_name}Loader\"")
+        return ", ".join(names)
+
     def _generate_module_loader_h(self, module_loader_h_dir: str, triplet_files: dict[str, list[str]]):
         module_loader_h_template_path = os.path.join(module_loader_h_dir, "ModuleLoader.h.in")
         with open(module_loader_h_template_path, "r") as file_in:
             module_loader_h_template = TemplatedString(file_in.read())
-        module_name = UeVcpkgExport._generate_module_name(self._package)
+        module_name = self.get_module_export_name()
 
         text = module_loader_h_template.substitute(
             {
                 "ModuleName": module_name,
-                "DynamicLibraryBinaryNames": UeVcpkgExport._generate_dynamic_library_binary_names(triplet_files)
+                "DynamicLibraryBinaryNames": UeVcpkgExport._generate_dynamic_library_binary_names(triplet_files),
+                "DependencyModuleLoaderNames": self._generate_dependency_module_loader_names()
             }
         )
         return text
@@ -244,8 +371,12 @@ class UeVcpkgExport:
         with open(module_loader_cpp_template_path, "r") as file_in:
             module_loader_cpp_template = TemplatedString(file_in.read())
 
-        module_name = UeVcpkgExport._generate_module_name(self._package)
-        text = module_loader_cpp_template.substitute({"ModuleName": module_name})
+        module_name = self.get_module_export_name()
+        text = module_loader_cpp_template.substitute(
+            {
+                "ModuleName": module_name,
+            }
+        )
         return text
 
     def build(self, triplet: str | None, overlay_triplet: str | None):
@@ -281,7 +412,7 @@ class UeVcpkgExport:
             info_package = info_segments[0]
             info_version = info_segments[1]
             info_triplet = info_segments[2]
-            if info_package != self._package:
+            if info_package != UeVcpkgExport.trim_vcpkg_name(self._package):
                 continue
             if info_triplet != triplet:
                 continue
@@ -324,18 +455,19 @@ class UeVcpkgExport:
 
     def export(
             self,
-            triplets: list[str],
             module_build_cs_dir: str,
             module_loader_build_cs_dir: str,
             module_loader_h_dir: str,
             module_loader_cpp_dir: str,
             output_dir: str,
-            fnmatch_patterns: list[str] | None,
-            extensions_whitelist: list[str] | None,
             overwrite: bool = False
     ):
-        module_name = UeVcpkgExport._generate_module_name(self._package)
-        module_loader_name = UeVcpkgExport._generate_module_name(self._package) + "Loader"
+        if not self.should_export_module():
+            self._logger.warning(f"Exporting disabled for {self._package} in package rules.")
+            return
+
+        module_name = self.get_module_export_name()
+        module_loader_name = module_name + "Loader"
 
         module_output_dir = os.path.join(output_dir, module_name)
         module_loader_output_dir = os.path.join(output_dir, module_loader_name)
@@ -358,19 +490,10 @@ class UeVcpkgExport:
         module_private_dir = os.path.join(module_output_dir, "Private")
         module_public_dir = os.path.join(module_output_dir, "Public")
 
-        module_loader_private_dir = os.path.join(module_loader_output_dir, "Private")
-        module_loader_public_dir = os.path.join(module_loader_output_dir, "Public")
-
         os.makedirs(module_private_dir)
         os.makedirs(module_public_dir)
 
-        os.makedirs(module_loader_private_dir)
-        os.makedirs(module_loader_public_dir)
-
-        triplet_files = {}
-        for triplet in triplets:
-            owned_files = self.get_owned_files(triplet, fnmatch_patterns, extensions_whitelist)
-            triplet_files[triplet] = owned_files
+        triplet_files = self.get_triplet_files()
 
         build_cs_text = self._generate_build_cs(module_build_cs_dir, triplet_files)
         build_cs_path = os.path.join(module_output_dir, f"{module_name}.Build.cs")
@@ -378,6 +501,11 @@ class UeVcpkgExport:
             file_out.write(build_cs_text)
 
         if UeVcpkgExport._is_loader_module_needed(triplet_files):
+            module_loader_private_dir = os.path.join(module_loader_output_dir, "Private")
+            module_loader_public_dir = os.path.join(module_loader_output_dir, "Public")
+            os.makedirs(module_loader_private_dir)
+            os.makedirs(module_loader_public_dir)
+
             loader_build_cs_text = self._generate_loader_build_cs(module_loader_build_cs_dir)
             module_loader_h_text = self._generate_module_loader_h(module_loader_h_dir, triplet_files)
             module_loader_cpp_text = self._generate_module_loader_cpp(module_loader_cpp_dir)
@@ -392,7 +520,7 @@ class UeVcpkgExport:
             with open(loader_build_cs_path, "w") as file_out:
                 file_out.write(loader_build_cs_text)
 
-        for triplet in triplets:
+        for triplet in self._triplets:
             files = triplet_files[triplet]
             triplet_output_dir = os.path.join(module_output_dir, triplet)
             for file in files:
@@ -430,58 +558,49 @@ def main():
     parser.add_argument("--with_dependencies", action="store_true")
     parser.add_argument("--overwrite", action="store_true")
 
-    parser.add_argument("--package_blacklist", default="package-blacklist.txt")
+    parser.add_argument("--package_rules", default="package-rules.json")
 
     args = parser.parse_args()
 
     logger = logging.getLogger("UeVcpkgExport")
     logger.setLevel(args.verbosity)
 
-    package_blacklist_path = args.package_blacklist
-    package_blacklist = []
-    if package_blacklist_path is not None:
-        with open(package_blacklist_path, "r") as package_blacklist_file:
-            package_blacklist = [line.strip() for line in package_blacklist_file.readlines()]
+    package_rules_path = args.package_rules
+    package_rules = []
+    if package_rules_path is not None:
+        with open(package_rules_path, "r") as package_rules_file:
+            package_rules = json.load(package_rules_file)["package_rules"]
 
     packages_dict = {}
-    export_obj = UeVcpkgExport(args.target_package, args.vcpkg_dir, packages_dict, logger)
+    export_obj = UeVcpkgExport(
+        args.target_package,
+        args.vcpkg_dir,
+        packages_dict,
+        package_rules,
+        args.triplets,
+        args.export_fnmatch,
+        args.export_extension,
+        logger)
     # export_obj.print_dependency_tree(lambda msg: logger.info(msg))
     for triplet in args.triplets:
         export_obj.build(triplet, args.overlay_triplets)
 
     if not args.with_dependencies:
         export_obj.export(
-            args.triplets,
             args.build_cs_templates,
             args.module_h_templates,
             args.module_cpp_templates,
             args.output_dir,
-            args.export_fnmatch,
-            args.export_extension,
             args.overwrite
         )
     else:
         for package_name, package_export in packages_dict.items():
-
-            blacklisted = False
-
-            for package_blacklist_entry in package_blacklist:
-                if re.match(package_blacklist_entry, package_name):
-                    logger.warning(f"Package {package_name} blacklisted.")
-                    blacklisted = True
-                    break
-
-            if blacklisted:
-                continue
             package_export.export(
-                args.triplets,
                 args.module_build_cs_templates,
                 args.loader_build_cs_templates,
                 args.module_loader_h_templates,
                 args.module_loader_cpp_templates,
                 args.output_dir,
-                args.export_fnmatch,
-                args.export_extension,
                 args.overwrite
             )
 
